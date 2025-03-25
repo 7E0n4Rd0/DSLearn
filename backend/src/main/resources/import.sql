8a6d7d78e3e498c0b6b3738abc240bf41014c424
Leonardo <leomeireles66@gmail.com>
2025-03-25 20:23:53 -0300

    Course

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -13,4 +13,6 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
 
+INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxVTgY2M7QJSWd1042mLImCtpzd-5DtItp1w&s', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRQQpQrZ7qKeztb958uVbGEyA56RfPTi_8tQ&s');
+
 
