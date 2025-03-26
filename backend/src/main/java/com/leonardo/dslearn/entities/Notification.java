da273679e81eea87e1665c44f23785b3aae37ed7
Leonardo <leomeireles66@gmail.com>
2025-03-26 20:59:06 -0300

    Enrollment, EnrollmentPK

--- backend/src/main/java/com/leonardo/dslearn/entities/Notification.java
+++ backend/src/main/java/com/leonardo/dslearn/entities/Notification.java
@@ -14,8 +14,10 @@ public class Notification implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String text;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private Instant moment;
-    private Boolean read;
+    private boolean read;
     private String route;
 
     @ManyToOne
